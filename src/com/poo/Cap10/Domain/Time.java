1c884f2111aef5b845ee78f496691d3817af8d33
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-03-09 10:00:16 -0300

    treinando-- exercicio -- Associação

--- src/com/poo/Cap10/Domain/Time.java
+++ src/com/poo/Cap10/Domain/Time.java
@@ -2,9 +2,24 @@ package com.poo.Cap10.Domain;
 
 public class Time {
     private String nome;
+//    adicionando varios jogadores
+    private Jogadores[] jogadores;
+
+    public Time(String nome, Jogadores[] jogadores) {
+        this.nome = nome;
+        this.jogadores = jogadores;
+    }
 
     public Time(String nome) {
         this.nome = nome;
+    }
+    public void imprime(){
+        System.out.println(this.nome);
+        if (jogadores == null)return;
+        for (Jogadores jogador : jogadores) {
+            System.out.println(jogador.getNome());
+        }
+
     }
 
     public String getNome() {
@@ -13,5 +28,13 @@ public class Time {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public Jogadores[] getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(Jogadores[] jogadores) {
+        this.jogadores = jogadores;
     }
 }
--- src/com/poo/Cap10/Test/JogadoresTest.java
+++ src/com/poo/Cap10/Test/JogadoresTest.java
@@ -5,7 +5,7 @@ import com.poo.Cap10.Domain.Time;
 
 public class JogadoresTest {
     public static void main(String[] args) {
-        Jogadores jogador1 = new Jogadores("tiririca");
+        //  Jogadores jogador1 = new Jogadores("tiririca");
         //  Jogadores jogador2 = new Jogadores("maracanã");
         //  Jogadores jogador3 = new Jogadores("ai papai");
 
@@ -13,11 +13,26 @@ public class JogadoresTest {
 //        for (Jogadores jogadores : jogador) {
 //            jogadores.imprime();
 //        }
-//        Fazenod a associação 1 -- N. Classe jogadores nao inicia novo objeto
+//        Fazendo a associação 1 -- N. Classe jogadores nao inicia novo objeto
 //        private Time time;
-        Time time = new Time("brasil");
-        jogador1.setTime(time);
-        jogador1.imprime();
+//        Time time = new Time("brasil");
+//        jogador1.setTime(time);
+//        jogador1.imprime();
+
+
+        //ASSOCIAÇÃO MUITOS PARA MUITOS
+        Jogadores jogadores = new Jogadores("tiririca");
+//        armazenando jogadores em um array
+        Jogadores[] jogador = {jogadores};
+
+        Time time = new Time("Time abestado");
+//        setando os jogadores e os times:
+        jogadores.setTime(time);
+        time.setJogadores(jogador);
+        System.out.println("--Times--");
+        time.imprime();
+        System.out.println("--jogadores--");
+        jogadores.imprime();
 
     }
 }
